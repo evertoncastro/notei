52a96b608cd7ee027ce96e5ecd8d1e5435e89e0a
evertmbh <evertmbh@hotmail.com>
2015-06-27 15:40:04 -0300

    setup karma part 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var karma = require('karma').server;
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
@@ -49,4 +50,14 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/tests/my.conf.js',
+    singleRun: true
+  }, function() {
+    done();
+  });
 });
