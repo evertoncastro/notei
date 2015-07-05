ac0e4d2d091d2040c76dc9d769723e0ac59d1350
evertmbh <evertmbh@hotmail.com>
2015-07-04 22:29:54 -0300

    tabs left and right on subject

--- www/js/controllers/controllerDashBoard.js
+++ www/js/controllers/controllerDashBoard.js
@@ -6,12 +6,26 @@ angular.module('starter').controller('DashBoardCtrl', DashBoardCtrl);
 function DashBoardCtrl($scope){
 
     $scope.showSubject = false;
+    $scope.showLeftTab = true;
+    $scope.showRightTab = false;
     $scope.init = function(){
 
     };
 
     $scope.openSubject = function(){
-        $scope.showSubject = !$scope.showSubject;
+      $scope.showSubject = !$scope.showSubject;
+    };
+
+    $scope.openLeftTab = function(){
+        $scope.showLeftTab = !$scope.showLeftTab;
+        $scope.showRightTab = !$scope.showRightTab;
+        console.log('Left');
+    };
+
+    $scope.openRightTab = function(){
+        $scope.showRightTab = !$scope.showRightTab;
+        $scope.showLeftTab = !$scope.showLeftTab;
+        console.log('Right');
     };
 
     $scope.init();
